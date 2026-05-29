08a03320733f9ff0d783075f9b6fc14acf506b77
CarlosVargasYam <carlosluisvargastenciocv@gmail.com>
2026-05-29 23:50:09 +0000

    GeneradorPokemonCarlosYam

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let who = ['Pikachu', 'Bulbasaur', 'Charmander', 'Squirtle'];
+let action = ['se comio', 'se meo', 'golpeo', 'rompio'];
+let what = ['mi cena', 'la pokeball', 'mis calcetines'];
+let when = ['antes de el entrenamiento', 'cuando estaba durmiendo', 'cuando estabamos en medio de una pelea', 'mientras hablaba con la enfermera Joy', 'Mientras estaba estudiando en la Pokedex'];
+
+  console.log("Buenas Buenas");
+
+
+
+
+let randomwho = Math.floor(Math.random() * who.length);
+let randomaction = Math.floor(Math.random() * action.length);
+let randomwhat = Math.floor(Math.random() * what.length);
+let randomwhen = Math.floor(Math.random() * when.length);
+
+let randomexcuse = who[randomwho] + " " + action[randomaction] + " " + what[randomwhat] + " " + when[randomwhen]
+
+let excuse = document.querySelector ("#excuse") 
+
+ excuse.innerHTML = randomexcuse ;
+}
